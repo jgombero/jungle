7dc0cda3c539b1ea844447c7bae6ae9fc4e34544
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-07-28 03:55:55 +0000

    Update create method to use authenticate_with_credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    
+    @user = User.authenticate_with_credentials(params[:email], params[:password])
 
-    # If the user exists AND the password entered is correct
-    if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user logged in when they navigate around our app.
+    if @user
+
       session[:user_id] = user.id
       redirect_to '/'
+
     else
-      # If user's login doesn't work, send them back to the login form
       redirect_to '/login', alert: "Invalid email and/or password"
     end
   end
@@ -22,5 +22,4 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to '/login'
   end
-
 end
